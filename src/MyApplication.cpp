677a935113c4b9011708b2143d56f963c361fe95
Tomochika Hara <tomochika1985@gmail.com>
2018-04-07 02:06:30 +0900

    Draw my first triangle

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -1,9 +1,10 @@
 #include <Magnum/DefaultFramebuffer.h>
 #include <Magnum/Platform/Sdl2Application.h>
-#include <Magnum/Context.h>
-#include <Magnum/Renderer.h>
-#include <Magnum/Version.h>
-#include <Magnum/Math/Color.h>
+#include <Magnum/Buffer.h>
+#include <Magnum/DefaultFramebuffer.h>
+#include <Magnum/Mesh.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Shaders/VertexColor.h>
 
 using namespace Magnum;
 
@@ -13,21 +14,37 @@ class MyApplication: public Platform::Application {
 
     private:
         void drawEvent() override;
+
+        Buffer _buffer;
+        Mesh _mesh;
+        Shaders::VertexColor2D _shader;
 };
 
 MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments} {
-    using namespace Magnum::Math::Literals;
+    using namespace Math::Literals;
 
-    Renderer::setClearColor(0xa5c9ea_rgbf);
+    struct TriangleVertex {
+        Vector2 position;
+        Color3 color;
+    };
+    const TriangleVertex data[]{
+            {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
+            {{ 0.5f, -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
+            {{ 0.0f,  0.5f}, 0x0000ff_rgbf}     /* Top vertex, blue color */
+    };
 
-    Debug{} << "Hello! This application is running on" << Context::current().version()
-            << "using" << Context::current().rendererString();
+    _buffer.setData(Containers::ArrayView<const void>(data, sizeof(data)), BufferUsage::StaticDraw);
+    _mesh.setPrimitive(MeshPrimitive::Triangles)
+            .setCount(3)
+            .addVertexBuffer(_buffer, 0,
+                             Shaders::VertexColor2D::Position{},
+                             Shaders::VertexColor2D::Color{Shaders::VertexColor2D::Color::Components::Three});
 }
 
 void MyApplication::drawEvent() {
     defaultFramebuffer.clear(FramebufferClear::Color);
 
-    /* TODO: Add your drawing code here */
+    _mesh.draw(_shader);
 
     swapBuffers();
 }
